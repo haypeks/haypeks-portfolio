f2e3ab7f63f0f59526aea7f662e9b5e3100a335a
Apeloko Timilehin <110821276+haypeks@users.noreply.github.com>
2024-02-22 22:09:15 +0000

    minor change

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,30 +4,22 @@ import portfolio from "../images/portfolio.png";
 import tipcalculator from "../images/tipcalculator.png";
 import matak from "../images/matak.png";
 import gamehub from "../images/gamehub.png";
+import kry8 from "../images/kry8.png";
 import { motion } from "framer-motion";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: manage,
-      heading: "Manage",
+      src: kry8,
+      heading: "Kry-8",
       hrefDemo: "https://eclectic-clafoutis-98a4e5.netlify.app",
       hrefCode: "https://github.com/haypeks/Landing-Page",
       paragraph:
-        "is a landing page for a business that helps software teams to plan day-to-day tasks while keeping the larger team goals in view. This is a front-end Mentor challenge to test responsive skills.  There are lots of small details and slight content shifts for different screen sizes.                  ",
+        " an AI writing website crafted with React for a seamless and dynamic writing experience. With a sleek and responsive design,  it combines the efficiency of React to offer a user-centric platform for innovative content creation. Embrace a new era of gradient in web design.                ",
     },
     {
       id: 2,
-      src: portfolio,
-      heading: "Portfolio",
-      hrefDemo: "https://haypeks-portfolio.netlify.app",
-      hrefCode: "https://github.com/haypeks/haypeks-portfolio",
-      paragraph:
-        "website was built using React and Tailwind. Harnessing the power of React, each component is carefully crafted to ensure a seamless, responsive, and engaging UI. Tailwind CSS allowed me to effortlessly create a visually striking design while the codebase are maintainable.                         ",
-    },
-    {
-      id: 3,
       src: matak,
       heading: "Matak",
       hrefDemo: "https://grand-concha-1e5ca0.netlify.app",
@@ -36,13 +28,23 @@ const Portfolio = () => {
         " is a responsive ecommerce website for a clothing business. Powered by the trio of HTML, CSS, and Vanilla JavaScript, this website is a testament to the elegance of simplicity. Built with users in mind, Matak offers a user-focused experience and responsive on all screen sizes.                    ",
     },
     {
-      id: 4,
-      src: tipcalculator,
-      heading: "Games",
-      hrefDemo: "https://tiny-pudding-d7cfb1.netlify.app",
-      hrefCode: "https://github.com/haypeks/Tip-calculator",
+      id: 3,
+      src: portfolio,
+      heading: "Portfolio",
+      hrefDemo: "https://haypeks-portfolio.netlify.app",
+      hrefCode: "https://github.com/haypeks/haypeks-portfolio",
       paragraph:
-        "Lorem Ipsium rure dolor in reprehenderit in voluptate velit esse cillum dolo. rure dolor in reprehenderit in voluptate velit esse cillum dolo. rure dolor in reprehenderit in voluptate velit esse cillum dolo.rure dolor in reprehenderit in voluptate velit esse cillum dolo                          ",
+        "website was built using React and Tailwind. Harnessing the power of React, each component is carefully crafted to ensure a seamless, responsive, and engaging UI. Tailwind CSS allowed me to effortlessly create a visually striking design while the codebase are maintainable.                         ",
+    },
+
+    {
+      id: 4,
+      src: manage,
+      heading: "Manage",
+      hrefDemo: "https://eclectic-clafoutis-98a4e5.netlify.app",
+      hrefCode: "https://github.com/haypeks/Landing-Page",
+      paragraph:
+        "is a landing page for a business that helps software teams to plan day-to-day tasks while keeping the larger team goals in view. This is a front-end Mentor challenge to test responsive skills.  There are lots of small details and slight content shifts for different screen sizes.                    ",
     },
     {
       id: 5,
@@ -98,7 +100,7 @@ const Portfolio = () => {
           </p>
         </motion.div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-x-16 gap-y-8 sm:px-0">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-x-16 gap-y-8 sm:px-0 ">
           {portfolios.map(
             ({ id, src, heading, hrefDemo, hrefCode, paragraph }) => (
               <motion.div
@@ -119,11 +121,11 @@ const Portfolio = () => {
                   <strong className="mr-1 font-bold">{heading}</strong>
                   {paragraph}
                 </p>
-                <div className="flex items-center gap-8 justify-evenly">
-                  <button className="bg-primary text-black w-fit text-xs cursor-pointer px-4 py-1 rounded border-b-2 border-black mt-4 md:font-bold font-medium hover:scale-105 hover:text-gray-700">
-                    <a href={hrefDemo}>Live Demo</a>
+                <div className="flex items-center gap-8 justify-evenly ">
+                  <button className="bg-primary text-black w-fit text-xs cursor-pointer px-4 py-1 rounded border-b-2 border-black mt-4 md:font-bold font-medium hover:scale-105 hover:text-gray-700 ">
+                    <a href={hrefDemo}>Demo</a>
                   </button>
-                  <button className="bg-primary text-black w-fit text-xs cursor-pointer px-4 py-1 rounded border-b-2 border-black mt-4 md:font-bold  font-medium hover:scale-105 hover:text-gray-700">
+                  <button className="bg-primary text-black w-fit text-xs cursor-pointer px-4 py-1 rounded border-b-2 border-black mt-4 md:font-bold  font-medium hover:scale-105 hover:text-gray-700 ">
                     <a href={hrefCode}>Code</a>
                   </button>
                 </div>
